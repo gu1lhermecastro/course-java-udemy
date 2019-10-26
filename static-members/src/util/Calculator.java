f01cfa544e20e18aba8d6b9c7ceae014971ab3dc
Guilherme Castro <guilhermecastro.developer@gmail.com>
2019-10-26 18:21:38 -0300

    Learning static members

--- static-members/src/util/Calculator.java
+++ static-members/src/util/Calculator.java
@@ -4,15 +4,12 @@ public class Calculator {
   public static double sum(double value1, double value2) {
     return (value1 + value2);
   }
-
   public static double division(double value1, double value2) {
     return (value1 / value2);
   }
-
   public static double subtraction(double value1, double value2) {
     return (value1 - value2);
   }
-
   public static double multiplication(double value1, double value2) {
     return (value1 * value2);
   }
